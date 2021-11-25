c876f542a19ff4b91e794197e6bc5ad84941aa5d
JD <jdmejia712@gmail.com>
2021-11-25 03:48:51 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+let textBox = document.querySelector('#new-task-description')
+let btn = document.querySelectorAll('input')[1]
+let formBox = document.querySelector('form')
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  formBox.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    handleToDo(e.target.new_task_description.value)
+
+    formBox.reset()
+  })
 });
+
+function handleToDo (todo){
+  let li = document.createElement('li')
+  li.textContent = todo
+
+  let btn = document.createElement('button')
+  btn.textContent = 'del'
+  
+  btn.addEventListener('click', deleteTask)
+  li.appendChild(btn)
+
+  document.querySelector('#tasks').appendChild(li)
+
+
+}
+  
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
+//reset()
